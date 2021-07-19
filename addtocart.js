44d07d65876fb1db5b54dd3097bf31bc5335edce
Aakash <aakashdinkarh@gmail.com>
2021-07-19 11:10:10 +0530

    add to cart functionality added

--- addtocart.js
+++ addtocart.js
@@ -1,20 +1,36 @@
-let storage = JSON.parse(localStorage.getItem('books'))
+window.onload = () => {
+  let addBtn = document.querySelectorAll(".cards .btn");
+  for (let i = 0; i < addBtn.length; i++) {
+    addBtn[i].addEventListener("click", (e) => {
+      console.log('btn clicked')
+      let a, b;
+      a = e.currentTarget.parentElement.children[1].innerHTML;
+      b = e.currentTarget.parentElement.children[2].innerHTML;
+      storeItem(a, b);
+    });
+  }
+}
+
 var arr = [];
-for( let i = 0; i < storage.length; i++){
+let storage = JSON.parse(localStorage.getItem("books"));
+for (let i = 0; i < storage.length; i++) {
   arr.push(storage[i]);
 }
 
-function storeItem(a = "Dummy", b = '$5.00') {
-  console.log('called')
-  //name, cost --> plz pass in this order
+function storeItem(a = "Dummy", b = "$5.00", c = 1) {
+  //name, cost, quantity --> plz pass in this order
   let item = {
     name: a,
-    cost: b
+    cost: b,
+    quantity: c,
   };
-  // for(let a = 0; a < arr.length; a++){
-  //   if(arr[a].name == playerNumber.name)
-  //   return alert('Player already in cart')
-  // }
+  for (let a = 0; a < arr.length; a++) {
+    if (arr[a].name == item.name) {
+      arr[a].quantity++;
+      localStorage.setItem("books", JSON.stringify(arr));
+      return;
+    }
+  }
   arr.push(item);
   localStorage.setItem("books", JSON.stringify(arr));
 }
@@ -22,19 +38,18 @@ function storeItem(a = "Dummy", b = '$5.00') {
 function getAllItem() {
   let getArr = JSON.parse(localStorage.getItem("books"));
   for (let i = 0; i < getArr.length; i++) {
-    addItem(getArr[i]);
+    addItem(getArr[i], i+1);
   }
 }
 
-function addItem(dum) {
+function addItem(dum, srNo) {
   let box = document.querySelector(".table-box");
 
-  let new_item = document.createElement('div');
-  box.append(new_item)
-  new_item.outerHTML = 
-    `<div class="table-row">
+  let new_item = document.createElement("div");
+  box.append(new_item);
+  new_item.outerHTML = `<div class="table-row">
     <div class="table-cell first-cell">
-        <p>9</p>
+        <p>${srNo}</p>
     </div>
     <div class="table-cell">
         <p>${dum.name}</p>
@@ -43,22 +58,23 @@ function addItem(dum) {
         <p>${dum.cost}</p>
     </div>
     <div class="table-cell last-cell">
-        <p>1</p>
+        <p>${dum.quantity}</p>
     </div>
-  </div>`
+    <div class="table-cell last-cell">
+    <button class="btn2">Remove</button>
+    </div>
+    </div>`;
 }
 
-// function removePlayer(p_name){
-//   let removeElem = this.parentElement.parentElement.parentElement;
-//   removeElem.remove();
-
-//   let z = JSON.parse(localStorage.getItem('array'));
-//   arr = [];
-//   for(let k = 0; k < Object.keys(z).length; k++){
-//     if(z[k].name == p_name) continue
-//     else{
-//       arr.push(z[k])
-//     }
-//   }
-//   localStorage.setItem('array',JSON.stringify(arr));
-// }
+function removeItem(item) {
+  item.remove();
+  let z = JSON.parse(localStorage.getItem("books"));
+  arr = [];
+  for (let k = 0; k < Object.keys(z).length; k++) {
+    if (z[k].name == item.children[1].children[0].innerHTML) continue;
+    else {
+      arr.push(z[k]);
+    }
+  }
+  localStorage.setItem("books", JSON.stringify(arr));
+}
